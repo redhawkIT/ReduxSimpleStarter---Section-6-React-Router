5bcd6f7a8bd0657c15ff5a3bba6370f3d9f3119a
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-09-21 11:15:29 -0500

    add child routes

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,16 @@ import { Route, IndexRoute } from 'react-router'
 
 import App from './components/app'
 
+// test component
+
+const Greeting = () => {
+  return <div>Testing 123</div>
+}
+
 export default (
-  <Route path="/" component={App} />
+  <Route path="/" component={App}>
+    <Route path="greet" component={Greeting} />
+    <Route path="greet2" component={Greeting} />
+    <Route path="greet3" component={Greeting} />
+  </Route>
 )
